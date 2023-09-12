0f1412793b672a44ece817e6d87edf236eb43a89
ZarwalD <mdogminec@gmail.com>
2023-09-12 10:55:12 +0700

    Добавьте файлы проекта.

---
+++ lab-2-p2/lab-2-p2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cout << "Number: ";
+	int num;
+	std::cin >> num;
+	int firstNumber = num / 100;
+	int secondNumber = (num / 10) % 10;
+	int thirdNumber = num % 10;
+	int sum = firstNumber + secondNumber + thirdNumber;
+	int mul = firstNumber * secondNumber * thirdNumber;
+	std::cout << "Sum = " << sum << "; Mul = " << mul << std::endl;
+}
